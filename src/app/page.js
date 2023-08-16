2dee23d24bb4f52c4ce11546739e15ad4321876e
thiago aguiar <thiago.aguiar879@gmail.com>
2023-08-16 11:00:32 -0400

    correçoes

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import HomeSection from '../../componets/HomeSection'
 import Explore from '../../componets/Explore'
 import Footer from '../../componets/Footer'
 
-
 export default function Home() {
   return (
     <>
